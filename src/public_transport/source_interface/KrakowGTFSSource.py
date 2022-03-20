4d7f464068bdf64b5ddf4574ea21e59385447d4e
Gazda70 <pjgazda@interia.pl>
2022-03-20 22:13:27 +0100

    Implement request methods and add URL constants..

--- src/public_transport/source_interface/KrakowGTFSSource.py
+++ src/public_transport/source_interface/KrakowGTFSSource.py
@@ -1,12 +1,19 @@
+import requests
+
 from public_transport.source_interface.GTFSSource import GTFSSource
 
 
 class KrakowGTFSSource(GTFSSource):
+
+    GTFS_STATIC='https://gtfs.ztp.krakow.pl/GTFS_KRK_A.zip'
+    GTFS_TRIP_UPDATES='https://gtfs.ztp.krakow.pl/TripUpdates_A.pb'
+    GTFS_ALERTS='https://gtfs.ztp.krakow.pl/ServiceAlerts_A.pb'
+
     def make_static_request(self):
-        pass
+        return requests.get(self.GTFS_STATIC)
 
     def make_trip_update_request(self):
-        pass
+        return requests.get(self.GTFS_TRIP_UPDATES)
 
     def make_alert_request(self):
-        pass
+        return requests.get(self.GTFS_ALERTS)
--- src/public_transport/source_interface/LondonGTFSSource.py
+++ src/public_transport/source_interface/LondonGTFSSource.py
@@ -1,12 +1,19 @@
+import requests
+
 from public_transport.source_interface.GTFSSource import GTFSSource
 
 
 class LondonGTFSSource(GTFSSource):
+
+    GTFS_STATIC='https://www.londontransit.ca/gtfsfeed/google_transit.zip'
+    GTFS_TRIP_UPDATES='gtfs.ltconline.ca/TripUpdate/TripUpdates.pb'
+    GTFS_ALERTS='gtfs.ltconline.ca/Alert/Alerts.pb'
+
     def make_static_request(self):
-        pass
+        return requests.get(self.GTFS_STATIC)
 
     def make_trip_update_request(self):
-        pass
+        return requests.get(self.GTFS_TRIP_UPDATES)
 
     def make_alert_request(self):
-        pass
+        return requests.get(self.GTFS_ALERTS)
